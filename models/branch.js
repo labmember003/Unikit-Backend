37b42d47da79a812daae57cafac34bfa5edb1e94
Zeomite <105901968+Zeomite@users.noreply.github.com>
2023-08-31 13:02:30 +0530

    Update branch.js

--- models/branch.js
+++ models/branch.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
-const Subject = require("./subject")
 const branchSchema = mongoose.Schema({
+  branchId: {
+    type: String,
+    required: true,
+  },
     branchName: {
       type: String,
       required: true,
     },
-    subjectList: [Subject.schema]
+    
 });
 module.exports = mongoose.model("Branch", branchSchema);
