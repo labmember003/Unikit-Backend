8ec30b5ac2a78ffeb65da2a9eb8468fae07dcaec
Zeomite <105901968+Zeomite@users.noreply.github.com>
2023-08-31 12:58:48 +0530

    Update subject.js
    updated subject schema

--- models/subject.js
+++ models/subject.js
@@ -1,20 +1,14 @@
 const mongoose = require("mongoose");
-const Notes = require("./notes");
-const Book = require("./book");
-const Playlist = require("./playlist")
-const Syllabus = require("./syllabus")
-const Paper = require("./paper")
 
 const subjectSchema = mongoose.Schema({
+  subjectId: {
+    type: String,
+    required: true,
+  },
     subjectName: {
       type: String,
       required: true,
     },
-    notesList: [Notes.schema],
-    bookList: [Book.schema],
-    playList: [Playlist.schema],
-    syllabusList: [Syllabus.schema],
-    paperList: [Paper.schema]
-
+    
 });
 module.exports = mongoose.model("Subject", subjectSchema);
