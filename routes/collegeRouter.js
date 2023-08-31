7783067c58208d175e9125a39fba455620c764b0
Zeomite <105901968+Zeomite@users.noreply.github.com>
2023-08-31 13:03:02 +0530

    Update collegeRouter.js

--- routes/collegeRouter.js
+++ routes/collegeRouter.js
@@ -1,11 +1,15 @@
 const express = require("express");
-const { putCollege } = require("../controllers/collegeController");
-const { getCollege } = require("../controllers/collegeController");
-const auth = require("../middlewares/auth");
+collegeController=require("../controllers/collegeController");
+
+const collegeList = require("../controllers/collegeController");
+//const auth = require("../middlewares/auth");
 
 const collegeRouter = express.Router();
 
-collegeRouter.get("/:id", auth, getCollege);
-collegeRouter.post("/", auth, putCollege);
+//collegeRouter.get("/:id", auth, getCollege);
+//collegeRouter.post("/", auth, putCollege);
+collegeRouter.get('/', collegeController.getListOfColleges);
+
+collegeRouter.get('/data', collegeController.getDataBasedOnQuery);
 
 module.exports = collegeRouter;
