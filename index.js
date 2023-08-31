7cd533b983bb5a26c607efbbb27cea80c2cdfbcd
Zeomite <105901968+Zeomite@users.noreply.github.com>
2023-08-31 13:03:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,35 +21,8 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
-
-
-
 app.use("/users", userRouter);
 app.use("/college", collegeRouter);
-app.post("/test", upload.single("photo"), function (req, res, next) {
-  // Access JSON data
-  const jsonData = JSON.parse(req.body.data);
-  console.log(jsonData);
-
-  // Access uploaded file
-  const file = req.file;
-  console.log(file);
-
-  // Handle file upload
-  if (file) {
-    // File uploaded
-    res.send({
-      success: true,
-      message: "File Uploaded",
-      data: jsonData, // Send back the received JSON data
-    });
-  } else {
-    // No file uploaded
-    res.status(400).send("No file uploaded.");
-  }
-});
-
-
 app.get("/", (req, res) => {
     res.send("Uni-Kit API From Falcon Lab");
 })
@@ -57,7 +30,7 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 3000;
 
 mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
+.then(() => {
     app.listen(port, () => {
         console.log(`Server running on ${port}, http://localhost:${port}`)}
     );
@@ -65,3 +38,4 @@ mongoose.connect('mongodb://localhost/mydatabase', { useNewUrlParser: true, useU
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
 });
+
