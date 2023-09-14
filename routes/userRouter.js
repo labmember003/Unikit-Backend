35aff5ef85ca1b77cd6e9be3911d9b9f34c6639a
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2023-09-14 22:16:23 +0530

    Update userRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { signin, signup } = require("../controllers/userController");
+const { signin, signup, googleOneTap } = require("../controllers/userController");
 const userRouter = express.Router();
 
 userRouter.post("/signup", signup);
+
+userRouter.post("/googleOneTap", googleOneTap);
 
 userRouter.post("/signin", signin)
 
