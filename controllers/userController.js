e354f94c53d28ee02603f4050b90a2a28ab1c58f
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2023-09-22 22:57:54 +0530

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -15,12 +15,14 @@ const signup = async (req, res) => {
       });
     }
     const hashedpassword = await bcrypt.hash(password, 10);
+    const token = jwt.sign({ email: result.email, id: result._id }, SECRET_KEY);
     const result = await userModel.create({
       username: username,
       email: email,
       password: hashedpassword,
+      token: token
     });
-    const token = jwt.sign({ email: result.email, id: result._id }, SECRET_KEY);
+    
     res.status(201).json({ user: result, token: token });
   } catch (error) {
     console.log(error);
