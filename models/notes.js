1e87692cbfb4f1e5b673dcdf07f9cefb4ced9751
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2023-10-05 20:43:19 +0530

    Update notes.js

--- models/notes.js
+++ models/notes.js
@@ -15,6 +15,16 @@ const notesSchema = mongoose.Schema({
     notesID:{
       type: String,
       required: true,
+    },
+    likeCount:{
+      type: Number,
+      required: true,
+      default: 0
+    },
+    dislikeCount:{
+      type: Number,
+      required: true,
+      default: 0
     }
 });
 module.exports = mongoose.model("Notes", notesSchema);
