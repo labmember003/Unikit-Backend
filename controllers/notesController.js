5414a0abb30bcf2177dcd95380987d6cae9893b1
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2023-10-07 16:39:28 +0530

    Update notesController.js

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -7,6 +7,9 @@ const incLikeCount = async (req, res) => {
         const likes = await notes.find({ notesID: notesid }).likeCount ;
         likes=likes+1
         const updates = await notes.findOneAndUpdate({ notesID: notesid },{likeCount: likes})
+        res.status(200).json({
+        message: "updated sucessfully",
+      });
     } catch (error) {
       console.log(error);
       res.status(500).json({
@@ -22,6 +25,9 @@ const incLikeCount = async (req, res) => {
         const likes = await notes.find({ notesID: notesid }).dislikeCount ;
         likes=likes+1
         const updates = await notes.findOneAndUpdate({ notesID: notesid },{dislikeCount: likes})
+        res.status(200).json({
+        message: "updated sucessfully",
+      });
     } catch (error) {
       console.log(error);
       res.status(500).json({
