de8ffa259517f2434456184c0048304fecbd8160
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2023-11-10 13:57:07 +0530

    Update and rename notesController.js to contentController.js

---
+++ controllers/contentController.js
@@ -0,0 +1,68 @@
+const content = require("../models/content");
+
+const incLikeCount = async (req, res) => {
+    
+    try {
+        const contentid = req.contentid;
+        const likes = await content.find({ contentID: contentid }).likeCount ;
+        likes=likes+1
+        const updates = await content.findOneAndUpdate({ contentID: contentid },{likeCount: likes})
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "Something went wrong",
+      });
+    }
+  };
+
+  const showdata = async (req, res) => {
+    try {
+      const {
+          subjectid,
+          contendId
+      } = req.query;
+
+      let query = {};
+
+      if (subjectid) {
+          query.collegeId = collegeId;
+      }
+
+      if (contentid) {
+        query.courseId = courseId;
+      }
+
+
+      let results = {};
+
+      if (subjectid) {
+          results = await content.findMany({ subjectID: subjectid});
+      }
+
+      if (contentid) {
+          results = await content.find({ contentID: contentid});
+      }
+      res.json(results);
+  } catch (error) {
+      console.log(error)
+      res.status(500).json({ error: 'An error occurred' });
+  }
+  };
+
+
+  const incDislikeCount = async (req, res) => {
+    
+    try {
+        const contentid = req.contentid;
+        const likes = await content.find({ contentID: contentid }).dislikeCount ;
+        likes=likes+1
+        const updates = await content.findOneAndUpdate({ contentID: contentid },{dislikeCount: likes})
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        message: "Something went wrong",
+      });
+    }
+  };
+
+  module.exports = { incLikeCount,incDislikeCount, showdata };
