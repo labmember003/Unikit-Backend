3c0294f467359cd3ecf179d9a2d13490438ed11d
ZeoMite <105901968+Zeomite@users.noreply.github.com>
2023-12-11 22:17:56 +0530

    Create appRouter.js

---
+++ routes/appRouter.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const appRouter = express.Router();
+
+const assetlinks =[{
+    "relation": ["delegate_permission/common.handle_all_urls"],
+    "target": {
+      "namespace": "android_app",
+      "package_name": "com.falcon.unikit",
+      "sha256_cert_fingerprints":
+      ["CB:32:18:30:38:78:FF:21:E8:BB:10:12:A2:44:C7:EB:14:57:E3:6A:8E:15:8B:56:1A:F9:26:E8:DF:FC:C0:59"]
+  }
+  }];
+
+appRouter.post('/assetlink.json', (req, res) => {
+    res.json(assetlinks);
+});
